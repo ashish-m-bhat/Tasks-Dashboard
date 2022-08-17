6723267da656874592c8929b9f176fa1944b0cef
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-08-17 16:50:00 +0530

    FIrst commit - Inital steup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GetAllTasks from './Components/GetAllTasks/GetAllTasks';
+
+function App() {
+  return (
+    <div className="App">
+      <GetAllTasks />
+      {/* <DisplayTasks /> */}
+    </div>
+  );
+}
+
+export default App;
