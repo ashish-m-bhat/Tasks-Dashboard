18978b23cd70d901c78d51fbc00c2f52fe96e7d2
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-08-20 13:13:26 +0530

    Added Unit Test

---
+++ src/Components/DisplayTasks/DisplayNotStartedTasks/DisplayNotStartedTasks.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import DisplayNotStartedTasks from "./DisplayNotStartedTasks"
+import { Provider } from 'react-redux';
+import reduxStore from '../../../Store';
+
+test('testform', ()=>{
+    // render(<DisplayNotStartedTasks />);
+    // const NewTaskButton = screen.getByText('+');
+    // expect(NewTaskButton).toBeInTheDocument();
+
+});
